ccdcf920dc0193652cdbd93f48e7fc6b5faebad2
riepj9547 <riepj9547@HRH-IC0027345.wrdsb.ca>
2018-12-04 08:58:32 -0500

    Merge origin/master
    Conflicts:
    	core/src/com/summative4currymen/game/Bullet.java
    	core/src/com/summative4currymen/game/ZombieGame.java

--- core/src/com/summative4currymen/game/ZombieGame.java
+++ core/src/com/summative4currymen/game/ZombieGame.java
@@ -7,11 +7,11 @@ import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
+import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator.FreeTypeFontParameter;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
-import com.badlogic.gdx.utils.Timer;
-import com.badlogic.gdx.utils.Timer.Task;
-import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 
 public class ZombieGame extends ApplicationAdapter {
@@ -125,13 +125,56 @@ public class ZombieGame extends ApplicationAdapter {
                 
                 
 
+        batch = new SpriteBatch();
+        img = new Texture("badlogic.jpg");
+        menuPic = new Texture("MenuPic.jpg");
+        startButton = new Texture("StartButton.png");
 
-		
-	}
-	
-	@Override
-	public void dispose () {
-		batch.dispose();
-	}
+        cam = new OrthographicCamera();
+        viewport = new FitViewport(800, 600, cam);
+        viewport.apply();
+
+        cam.position.x = 400;
+        cam.position.y = 300;
+        cam.update();
+
+        //player1 = new Player();
+        //player2 = new Player();
+
+        FreeTypeFontGenerator generator = new FreeTypeFontGenerator(Gdx.files.internal("Xcelsion Italic.ttf"));
+        FreeTypeFontParameter parameter = new FreeTypeFontParameter();
+        parameter.size = 30;
+        font12 = generator.generateFont(parameter); // font size 12 pixels
+        generator.dispose();
+    }
+
+    @Override
+    public void render() {
+        Gdx.gl.glClearColor(1, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        //if the game has not started yet, draw in the main menu
+        if (startGame == false) {
+            shapeBatch.setProjectionMatrix(cam.combined);
+            shapeBatch.begin(ShapeRenderer.ShapeType.Filled);
+            batch.begin();
+            //the menu picture
+            batch.draw(menuPic, 0, 0, viewport.getWorldWidth(), viewport.getWorldHeight());
+            batch.end();
+            //if the gmae has begon draw in the game
+        } else if (startGame == true) {
+            shapeBatch.setProjectionMatrix(cam.combined);
+            shapeBatch.begin(ShapeRenderer.ShapeType.Filled);
+            batch.begin();
+            shapeBatch.rect(0, 0, viewport.getWorldWidth(), viewport.getWorldHeight());
+            font12.draw(batch, "What's up Gared", 50, 100);
+            batch.end();
+        }
+
+    }
+
+    @Override
+    public void dispose() {
+        batch.dispose();
+    }
 
 }
