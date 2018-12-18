16a4b44b8921c8738fd7b1df0aead8479db44772
riepj9547 <riepj9547@HRH-IC0027345.wrdsb.ca>
2018-12-18 08:11:31 -0500

    Merge origin/master
    Conflicts:
    	core/src/com/summative4currymen/game/ZombieGame.java

--- core/src/com/summative4currymen/game/Weapon.java
+++ core/src/com/summative4currymen/game/Weapon.java
@@ -76,14 +76,16 @@ public class Weapon {
 
         if (roation == 0) {
             if (numBullets == 1) {
-                Bullet b = new Bullet((int) x, (int) y, 10, 10, bulletSpeed, damage, 1, 0);
-                System.out.println("Made bullet");
+                Bullet b = new Bullet((int) x, (int) y, 10, 10, bulletSpeed, damage, (float)0.5, 0);
+                System.out.println("Made bullet");       
+                bullets.clear();
                 bullets.add(b);
                 return bullets;
             } else {
+                bullets.clear();
                 Bullet[] bArray = new Bullet[numBullets - 1];
                 for (int i = 0; i < numBullets; i++) {
-                    Bullet b = new Bullet((int) x, (int) y, 10, 10, bulletSpeed, damage, 1, (float) (Math.random() * (0.25 - 0)) + 0);
+                    Bullet b = new Bullet((int) x, (int) y, 10, 10, bulletSpeed, damage, (float) (Math.random() * (1 - 0.5)) + (float) 0.5, (float) (Math.random() * (0.25 - 0)) + 0);
                     System.out.println("Bullet" + i);
                     bullets.add(b);
                 }
@@ -92,13 +94,15 @@ public class Weapon {
         }
         if (roation == 45) {
             if (numBullets == 1) {
-                Bullet b = new Bullet((int) x, (int) y, 10, 10, bulletSpeed, damage, 1, 1);
+                Bullet b = new Bullet((int) x, (int) y, 10, 10, bulletSpeed, damage, (float)0.5, (float)0.5);
                 System.out.println("Made bullet");
+                bullets.clear();
                 bullets.add(b);
                 return bullets;
             } else {
+                bullets.clear();
                 for (int i = 0; i < numBullets; i++) {
-                    Bullet b = new Bullet((int) x, (int) y, 10, 10, bulletSpeed, damage, (float) (Math.random() * (1.25 - 0.75)) + 1, (float) (Math.random() * (1.25 - 0.75)) + 1);
+                    Bullet b = new Bullet((int) x, (int) y, 10, 10, bulletSpeed, damage, (float) (Math.random() * (1 - 0.25)) + (float)0.5, (float) (Math.random() * (1 - 0.25)) + (float)0.5);
                     System.out.println("Bullet" + i);
                     bullets.add(b);
                 }
@@ -107,13 +111,15 @@ public class Weapon {
         }
         if (roation == 90) {
             if (numBullets == 1) {
-                Bullet b = new Bullet((int) x, (int) y, 10, 10, bulletSpeed, damage, 0, 1);
+                Bullet b = new Bullet((int) x, (int) y, 10, 10, bulletSpeed, damage, 0, (float)0.5);
                 System.out.println("Made bullet");
+                bullets.clear();
                 bullets.add(b);
                 return bullets;
             } else {
+                bullets.clear();
                 for (int i = 0; i < numBullets; i++) {
-                    Bullet b = new Bullet((int) x, (int) y, 10, 10, bulletSpeed, damage, (float) (Math.random() * (0.25 - 0)) + 0, 1);
+                    Bullet b = new Bullet((int) x, (int) y, 10, 10, bulletSpeed, damage, (float) (Math.random() * (0.25 - 0)) + 0, (float) (Math.random() * (1.15 - 0.85)) + (float) 1);
                     System.out.println("Bullet" + i);
                     bullets.add(b);                    
                 }
@@ -122,11 +128,13 @@ public class Weapon {
         }
         if (roation == 135) {
             if (numBullets == 1) {
-                Bullet b = new Bullet((int) x, (int) y, 10, 10, bulletSpeed, damage, -1, 1);
+                Bullet b = new Bullet((int) x, (int) y, 10, 10, bulletSpeed, damage, (float)-0.5, (float)0.5);
                 System.out.println("Made bullet");
+                bullets.clear();
                 bullets.add(b);
                 return bullets;
             } else {
+                bullets.clear();
                 for (int i = 0; i < numBullets; i++) {
                     Bullet b = new Bullet((int) x, (int) y, 10, 10, bulletSpeed, damage, (float) (Math.random() * (-1.25 + 0.75)) - 1, (float) (Math.random() * (1.25 - 0.75)) + 1);
                     System.out.println("Bullet" + i);
@@ -137,11 +145,13 @@ public class Weapon {
         }
         if (roation == 180) {
             if (numBullets == 1) {
-                Bullet b = new Bullet((int) x, (int) y, 10, 10, bulletSpeed, damage, -1, 0);
+                Bullet b = new Bullet((int) x, (int) y, 10, 10, bulletSpeed, damage, (float)-0.5, 0);
                 System.out.println("Made bullet");
+                bullets.clear();
                 bullets.add(b);
                 return bullets;
             } else {
+                bullets.clear();
                 for (int i = 0; i < numBullets; i++) {
                     Bullet b = new Bullet((int) x, (int) y, 10, 10, bulletSpeed, damage, -1, (float) (Math.random() * (0.25 - 0)) + 0);
                     System.out.println("Bullet" + i);
@@ -151,12 +161,15 @@ public class Weapon {
             }
         }
         if (roation == 225) {
+            System.out.println("225 HERE BOIS");
             if (numBullets == 1) {
-                Bullet b = new Bullet((int) x, (int) y, 10, 10, bulletSpeed, damage, -1, -1);
+                Bullet b = new Bullet((int) x, (int) y, 10, 10, bulletSpeed, damage, (float)-0.5, (float)-0.5);
                 System.out.println("Made bullet");
+                bullets.clear();
                 bullets.add(b);
                 return bullets;
             } else {
+                bullets.clear();
                 for (int i = 0; i < numBullets; i++) {
                     Bullet b = new Bullet((int) x, (int) y, 10, 10, bulletSpeed, damage, (float) (Math.random() * (-1.25 + 0.75)) - 1, (float) (Math.random() * (-1.25 + 0.75)) - 1);
                     System.out.println("Bullet" + i);
@@ -167,11 +180,13 @@ public class Weapon {
         }
         if (roation == 270) {
             if (numBullets == 1) {
-                Bullet b = new Bullet((int) x, (int) y, 10, 10, bulletSpeed, damage, 0, -1);
+                Bullet b = new Bullet((int) x, (int) y, 10, 10, bulletSpeed, damage, 0, (float)-0.5);
                 System.out.println("Made bullet");
+                bullets.clear();
                 bullets.add(b);
                 return bullets;
             } else {
+                bullets.clear();
                 for (int i = 0; i < numBullets; i++) {
                     Bullet b = new Bullet((int) x, (int) y, 10, 10, bulletSpeed, damage, (float) (Math.random() * (0.25 - 0)) + 0, -1);
                     System.out.println("Bullet" + i);
@@ -181,12 +196,15 @@ public class Weapon {
             }
         }
         if (roation == 315) {
+            System.out.println("315 HERE BOIS");
             if (numBullets == 1) {
-                Bullet b = new Bullet((int) x, (int) y, 10, 10, bulletSpeed, damage, 1, -1);
+                Bullet b = new Bullet((int) x, (int) y, 10, 10, bulletSpeed, damage, (float)0.5, (float)-0.5);
                 System.out.println("Made bullet");
+                bullets.clear();
                 bullets.add(b);
                 return bullets;
             } else {
+                bullets.clear();
                 for (int i = 0; i < numBullets; i++) {
                     Bullet b = new Bullet((int) x, (int) y, 10, 10, bulletSpeed, damage, (float) (Math.random() * (1.25 - 0.75)) + 1, (float) (Math.random() * (-1.25 + 0.75)) - 1);
                     System.out.println("Bullet" + i);
