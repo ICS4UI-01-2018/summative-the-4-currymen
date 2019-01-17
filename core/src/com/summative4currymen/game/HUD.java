188bf1534ca01d7979898529002f5f1bc4f63ca3
goodm9679 <goodm9679@HRH-IC0027342.wrdsb.ca>
2019-01-17 08:16:19 -0500

    coin pickup functionality introduced HUD updated significantly

--- core/src/com/summative4currymen/game/HUD.java
+++ core/src/com/summative4currymen/game/HUD.java
@@ -3,6 +3,7 @@ package com.summative4currymen.game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
@@ -17,6 +18,10 @@ public class HUD {
     
     private float width;
     private BitmapFont font;
+    
+    private Texture shotgunIMG;
+    private Texture barretIMG;
+    private Texture akIMG;
 
     public HUD(float width) {
         this.width = width;
@@ -27,6 +32,9 @@ public class HUD {
         p.size = 17;
         font = g.generateFont(p);
         g.dispose();
+        shotgunIMG = new Texture("shotgun.png");
+        barretIMG = new Texture("RSASS_Sideview.png");
+        akIMG = new Texture("ak.png");
     }
 
     public void draw(ShapeRenderer shapeBatch, SpriteBatch batch, Player p1,OrthographicCamera cam) { //draw the HUD at 0,0
@@ -40,17 +48,19 @@ public class HUD {
             shapeBatch.setColor(1f, p1.getHealth() / 50f, 0f, 1);
         }
         shapeBatch.rect(cam.position.x-(cam.viewportWidth/2)+8, cam.position.y-(cam.viewportHeight/2)+8, ((this.width) - 16)*(p1.getHealth()/100.0f), 30);
-        if(p1.getEquipped() == "ShotGun"){
-            //draw shotgun
-        }else if(p1.getEquipped() == "Barret50"){
-            //draw barret
-        }
+        
         shapeBatch.end();
 
         batch.begin();
+        if(p1.getEquipped() == "ShotGun"){
+            batch.draw(shotgunIMG, cam.position.x-(cam.viewportWidth/2) + 12, cam.position.y-(cam.viewportHeight/2) + 50, 100, 40);
+        }else if(p1.getEquipped() == "Barret50"){
+            batch.draw(barretIMG, cam.position.x-(cam.viewportWidth/2) + 12, cam.position.y-(cam.viewportHeight/2) + 50, 100, 40);
+        }else if(p1.getEquipped() == "AK47"){
+            batch.draw(akIMG, cam.position.x-(cam.viewportWidth/2) + 12, cam.position.y-(cam.viewportHeight/2) + 50, 700, 50);
+        }
         font.setColor(Color.GOLD);
         font.draw(batch, "$" + p1.getCoins(), cam.position.x-(cam.viewportWidth/2) + 310, cam.position.y-(cam.viewportHeight/2) + 70);
-        
         batch.end();
     }
     
